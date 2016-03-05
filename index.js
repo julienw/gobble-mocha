388a3ebd8c6ff3db403957cc9dfe832da8140bfd
Julien Wajsberg <felash@gmail.com>
2016-03-05 14:43:08 +0100

    Default to running tests in the `test` directory

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ module.exports = function mocha ( inputdir, options, cb ) {
 
   var mocha = new Mocha( options );
 
+  options.testDir = options.testDir || 'test';
   mocha.files = sander.readdirSync( inputdir, options.testDir ).filter( n => n.endsWith('.js') ).map(function ( file ) {
     return path.join( inputdir, options.testDir, file );
   });
